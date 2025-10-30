e71e69793e5cb4ff32db619b03805d59cd30d960
IONUT\ivsch <ivscheianu@gmail.com>
2025-10-30 20:04:53 +0200

    Added Car class

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+    private int  id;
+    private String model;
+    private String color;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
